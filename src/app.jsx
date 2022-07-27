0f606f543eb36e5c25e9d131610d34c3ca8c437a
unknown <eodrmfdl1004@naver.com>
2022-07-28 00:38:39 +0900

    add footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './app.css';
+import { Box } from '@mui/material';
 import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Main />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+    <Box sx={{maxWidth: '1350px', m: 'auto'}}>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path='/' element={
+            <Main />
+          }></Route>
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </Box>
+  )
+};
