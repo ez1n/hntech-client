b33f5581fe6eabbe8e781d019b75ce8d46f9304c
unknown <eodrmfdl1004@naver.com>
2022-07-29 00:58:19 +0900

    udpate header, footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,77 +1,111 @@
 import React, { useState } from 'react';
-import styles from './header.module.css';
-import { Toolbar, Typography, Button, Stack, Box, Menu, MenuItem } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import './style.css';
+import { Toolbar, Typography, Button, Stack, Box, Paper } from '@mui/material';
 
 export default function Header(props) {
-  const [anchorCompany, setAnchorCompany] = useState(null);
-  const [anchorProduct, setAnchorProduct] = useState(null);
-  const [anchorService, setAnchorService] = useState(null);
+  const navigate = useNavigate();
 
-  const openCompany = Boolean(anchorCompany);
-  const openProduct = Boolean(anchorProduct);
-  const openService = Boolean(anchorService);
+  // 드롭다운 메뉴 open state
+  const [openCompany, setOpenCompany] = useState(false);
+  const [openProduct, setOpenProduct] = useState(false);
+  const [openService, setOpenService] = useState(false);
 
-  function MenuButton(props) {
+  // 하위 메뉴 component
+  function DropdownMenu(props) {
     return (
-      <Button onMouseOver={props.onHover} sx={{ fontSize: '1.2em', color: '#0F0F0F' }}>
-        {props.menu}
+      <Button
+        sx={{
+          justifyContent: 'center',
+          fontSize: 13,
+          color: '#0F0F0F',
+          '&:hover': {
+            backgroundColor: 'rgba(57, 150, 82, 0.2)',
+          },
+        }}>
+        {props.item}
       </Button>
     )
   };
 
+  // 페이지 이동 이벤트
+  const onClickMenu = (menu) => { navigate(menu) };
+
   return (
-    <Toolbar sx={{ p: 2, position: 'sticky', top: 0 }}>
+    <Toolbar sx={{ pt: 2, pb: 2, position: 'sticky', top: 0 }}>
       {/* 로고 */}
-      <Button sx={{ pl: 5, pr: 5 }}>
-        <img className={styles.logoImage} src='/images/logo.png' alt='HNTECH logo' />
+      <Button sx={{ pl: 5, pr: 5 }} onClick={() => onClickMenu('/')}>
+        {/* 로고 이미지 - 서버에서 받아오기 */}
+        <img className='logoImage' src='/images/logo.png' alt='HNTECH logo' />
         <Typography sx={{ ml: 3, fontSize: '2.5em', fontWeight: 'bold', color: '#0F0F0F' }}>HNTECH</Typography>
       </Button>
 
       {/* 메뉴 */}
       <Stack direction='row' sx={{ width: '100%', justifyContent: 'space-around' }}>
-        <Box>
-          <MenuButton
-            menu={'회사소개'}
-            onHover={event => setAnchorCompany(event.currentTarget)} />
-          <Menu
-            anchorEl={anchorCompany}
-            open={openCompany}
-            onMouseLeave={() => setAnchorCompany(null)}>
-            <MenuItem sx={{justifyContent: 'center'}}>인사말</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>회사 연혁</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>조직도</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>CI 소개</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>찾아오시는 길</MenuItem>
-          </Menu>
+        {/* 회사소개 */}
+        <Box onMouseLeave={() => setOpenCompany(!openCompany)}>
+          <Button
+            onMouseOver={() => setOpenCompany(!openCompany)}
+            sx={{ fontSize: '1.3em', color: '#0F0F0F' }}>
+            회사소개
+          </Button>
+          {openCompany &&
+            <Paper
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'absolute'
+              }}>
+              <DropdownMenu item={'인사말'} />
+              <DropdownMenu item={'회사 연혁'} />
+              <DropdownMenu item={'조직도'} />
+              <DropdownMenu item={'CI 소개'} />
+              <DropdownMenu item={'찾아오시는 길'} />
+            </Paper>
+          }
         </Box>
 
-        <Box>
-          <MenuButton
-            menu={'제품소개'}
-            onHover={event => setAnchorProduct(event.currentTarget)} />
-          <Menu
-            anchorEl={anchorProduct}
-            open={openProduct}
-            onMouseLeave={() => setAnchorProduct(null)}>
-            <MenuItem sx={{justifyContent: 'center'}}>스프링클러헤드</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>유수제어밸브</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>기타</MenuItem>
-          </Menu>
+        {/* 제품소개 */}
+        <Box onMouseLeave={() => setOpenProduct(false)}> 
+          <Button
+            onMouseOver={() => setOpenProduct(true)}
+            sx={{ fontSize: '1.3em', color: '#0F0F0F' }}>
+            제품소개
+          </Button>
+          {openProduct &&
+            <Paper
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'absolute',
+              }}>
+              <DropdownMenu item={'스프링클러헤드'} />
+              <DropdownMenu item={'유수제어밸브'} />
+              <DropdownMenu item={'기타'} />
+            </Paper>
+          }
         </Box>
 
-        <Box>
-          <MenuButton
-            menu={'고객지원'}
-            onHover={event => setAnchorService(event.currentTarget)} />
-          <Menu
-          anchorEl={anchorService}
-            open={openService}
-            onMouseLeave={() => setAnchorService(null)}>
-            <MenuItem sx={{justifyContent: 'center'}}>공지사항</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>자료실</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>고객문의</MenuItem>
-            <MenuItem sx={{justifyContent: 'center'}}>카다록 및 자재승인서</MenuItem>
-          </Menu>
+        {/* 고객지원 */}
+        <Box onMouseLeave={() => setOpenService(!openService)}>
+          <Button
+            onMouseOver={() => setOpenService(!openService)}
+            sx={{ fontSize: '1.3em', color: '#0F0F0F' }}>
+            고객지원
+          </Button>
+          {openService &&
+            <Paper
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                position: 'absolute',
+              }}>
+              <DropdownMenu item={'공지사항'} />
+              <DropdownMenu item={'자료실'} />
+              <DropdownMenu item={'카다록 및 자재승인서'} />
+              <DropdownMenu item={'고객문의'} />
+            </Paper>
+          }
         </Box>
       </Stack>
     </Toolbar>
